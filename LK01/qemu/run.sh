3fc100499bb02ad00867fb46d6b572d4f58bd600
hoanghailongvn <hoanghailong2642k@gmail.com>
2024-07-01 07:11:57 -0400

    init

---
+++ LK01/qemu/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+qemu-system-x86_64 \
+    -m 64M \
+    -nographic \
+    -kernel bzImage \
+    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 nopti nokaslr nopti" \
+    -no-reboot \
+    -cpu qemu64 \
+    -smp 1 \
+    -monitor /dev/null \
+    -initrd rootfs_updated.cpio \
+    -net nic,model=virtio \
+    -net user \
+    -gdb tcp::12345 \
+    -cpu kvm64,+smep,+smap
