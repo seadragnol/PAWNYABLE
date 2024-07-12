f4a732b649443037f1159cec1219ec44e38984e1
hoanghailongvn <hoanghailong2642k@gmail.com>
2024-07-12 00:39:57 -0400

    kaslr

--- LK01/qemu/kaslr_transfer.sh
+++ LK01/qemu/kaslr_transfer.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+musl-gcc kaslr_exploit.c -o exploit -static
+mv exploit root
 cd root; find . -print0 | cpio -o --null --owner=root --format=newc > ../debugfs.cpio
 cd ../
 
@@ -7,13 +8,12 @@ qemu-system-x86_64 \
     -m 64M \
     -nographic \
     -kernel bzImage \
-    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 nopti nokaslr nopti" \
+    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 pti=on" \
     -no-reboot \
-    -cpu qemu64 \
+    -cpu kvm64,+smep,+smap \
+    -gdb tcp::12345 \
     -smp 1 \
     -monitor /dev/null \
-    -initrd rootfs_updated.cpio \
+    -initrd debugfs.cpio \
     -net nic,model=virtio \
-    -net user \
-    -gdb tcp::12345 \
-    -cpu kvm64,+smep,+smap
+    -net user
