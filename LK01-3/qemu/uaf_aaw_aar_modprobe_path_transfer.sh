1e1fe459aeea2839779b90812ddd215e9a1153d1
hoanghailongvn <hoanghailong2642k@gmail.com>
2024-09-19 01:17:06 -0400

    update uaf_aaw_aar_modprobe_path_transfer.sh: enable kaslr

--- LK01-3/qemu/uaf_aaw_aar_modprobe_path_transfer.sh
+++ LK01-3/qemu/uaf_aaw_aar_modprobe_path_transfer.sh
@@ -22,7 +22,7 @@ qemu-system-x86_64 \
     -m 64M \
     -nographic \
     -kernel bzImage \
-    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 pti=on nokaslr" \
+    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 pti=on kaslr" \
     -no-reboot \
     -cpu qemu64,+smep,+smap \
     -smp 1 \
